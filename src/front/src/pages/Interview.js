016c18c847753b7f94abff8d829659cbc4ad8c04
JiminK <jimin981005@gmail.com>
2022-03-11 21:34:08 +0900

    set router

---
+++ src/front/src/pages/Interview.js
@@ -0,0 +1,11 @@
+const Interview = () => {
+    return (
+        <div>
+            <h1>
+                Interview Page!
+            </h1>
+        </div>
+    );
+};
+
+export default Interview;
