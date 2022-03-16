f6160ce7a8b028fa00c84d37975a212baed5c640
yoosun <39540525+usun813@users.noreply.github.com>
2022-03-17 02:55:42 +0900

    Add HomPage/components/Navbar/*  - 네비게이션바 파일 분리

---
+++ src/front/src/pages/HomePage/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: '소개',
+        url: '/introduce',
+        cName: 'nav-links'
+    },
+    {
+        title: '면접연습',
+        url: "/preinterview",
+        cName: 'nav-links'
+    },
+    {
+        title: '면접꿀팁',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '/mypage',
+        cName: 'nav-links-mobile'
+    }
+]
