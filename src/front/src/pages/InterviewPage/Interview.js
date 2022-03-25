7278afcce602d642ca9bf688dc7f7250ddee8815
JiminK <jimin981005@gmail.com>
2022-03-25 21:47:25 +0900

    make new branch

--- src/front/src/pages/InterviewPage/Interview.js
+++ src/front/src/pages/InterviewPage/Interview.js
@@ -27,13 +27,13 @@ function Interview() {
 
     return (
         <div className="interview-app">
-            {/*------ 가상 면접관 ------*/}
+            {/*------------------ 가상 면접관 ------------------*/}
             <div className="interviewer-section">
                 <video autoPlay width="100%" height="100%" controls >
                     <source src="./test_video.mp4" type="video/mp4"/>
                 </video>                
             </div>
-            {/*-------- 사용자 --------*/}
+            {/*-------------------- 사용자 --------------------*/}
             <div className="button-section">
                 {/* state true : 면접 시작 button */}
                 {buttonState 
