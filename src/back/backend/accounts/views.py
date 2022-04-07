8fd1786c0a1f4ee332a59301b2d9405b786312ca
yeieun1213 <yeieun1213@kookmin.ac.kr>
2022-04-08 00:08:09 +0900

    arrangement

--- src/back/backend/accounts/views.py
+++ src/back/backend/accounts/views.py
@@ -5,7 +5,7 @@ from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import CreateUserSerializer, UserSerializer, LoginUserSerializer
+from .serializers import CreateUserSerializer, LoginUserSerializer
 
 
 class SignupView(APIView):
