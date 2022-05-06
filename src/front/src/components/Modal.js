33945d1879b41c4f09ab6269089786535c72cade
JiminK <jimin981005@gmail.com>
2022-05-06 13:15:37 +0900

    Modal template

---
+++ src/front/src/components/Modal.js
@@ -0,0 +1,26 @@
+// 1111 make modal component
+
+import React, { useState, useEffect } from "react"; 
+import Modal from 'react-modal';
+
+const DefaultContent = () => {
+    return(
+        <div>hello</div>
+    )
+}
+
+const ModalComponent = ({
+    isOpen,
+    closeModalHandler,
+    modalStyle,
+    Content,
+    CloseButtonStyle
+}) => {
+    return (
+        <div className="modal">
+        </div>
+    );
+}
+
+
+export default ModalComponent;
