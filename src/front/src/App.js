ad8a79785b6f2d6d0e3e8cb993da355d1d9920da
yoosun <39540525+usun813@users.noreply.github.com>
2022-05-10 15:04:26 +0900

    App.js - Feedback 추가...

--- src/front/src/App.js
+++ src/front/src/App.js
@@ -5,7 +5,11 @@ import Interview from './pages/InterviewPage/Interview';
 import Mypage from './pages/MyPage/Mypage';
 import Login from './pages/LoginPage/Login';
 import Signup from './pages/LoginPage/Signup';
-import Feedback from './pages/MyPage/Feedback';
+import Feedback1 from './pages/MyPage/Feedback';
+import Feedback2 from './pages/MyPage/Feedback2';
+import Feedback3 from './pages/MyPage/Feedback3';
+import Feedback4 from './pages/MyPage/Feedback4';
+import Feedback5 from './pages/MyPage/Feedback5';
 function App() {
   return (
     <Routes>
@@ -13,9 +17,15 @@ function App() {
       <Route path="/preinterview" element={<PreInterview />} />
       <Route path="/interview" element={<Interview />} />
       <Route path="/mypage" element={<Mypage />} />
-      <Route path="/feedback" element={<Feedback />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup/>} />
+
+      <Route path="/feedback" element={<Feedback1 />} />
+      <Route path="/feedback/2" element={<Feedback2 />} />
+      <Route path="/feedback/3" element={<Feedback3 />} />
+      <Route path="/feedback/4" element={<Feedback4 />} />
+      <Route path="/feedback/5" element={<Feedback5 />} /> 
+      
     </Routes>
   );
 }
