edc34c832a1d72fe7b938709a7131fa0bfde8435
JiminK <jimin981005@gmail.com>
2022-05-10 14:36:18 +0900

    add interview type

--- src/front/src/pages/PreInterviewPage/PreInterview.js
+++ src/front/src/pages/PreInterviewPage/PreInterview.js
@@ -1,20 +1,27 @@
 // import Button from ''
 import './PreInterview.css';
+import styles from './SelectBox.module.css';
 import { PageDescription } from '../../components/PageDescription';
-// import { CamRecorder } from '../../components/CamRecorder';
-import React, { useEffect, useState } from "react"; 
+import WebcamStreamCapture from '../../components/Webcam'
+import React, { useEffect, useState, useRef, useCallback } from "react"; 
 import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
 import ModalComponent from '../../components/Modal';
-import Webcam from "react-webcam";
-import ReactDOM from 'react-dom';
+import SelectBox from '../../components/SelectBox';
 // import Footer from '../components/Footer';
 
 
 const clickMotion = () => window.open('/interview', '_blank');
 
-
-
+const interviewTypeName = {
+    0: '인성면접',
+    1: '임베디드',
+    2: 'UI/UX',
+    3: '데이터베이스',
+    4: '보안',
+    5: '클라우드',
+    6: '빅 데이터'   
+}
 
 const CamSetting = () => { 
        
