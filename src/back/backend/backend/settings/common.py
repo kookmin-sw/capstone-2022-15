e8814526cc7117caa84ad4605de865d7d2915493
yeieun1213 <yeieun1213@kookmin.ac.kr>
2022-05-11 10:50:18 +0900

    practice page api 수정 필요(user id json error)

--- src/back/backend/backend/settings/common.py
+++ src/back/backend/backend/settings/common.py
@@ -152,8 +152,9 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         #"rest_framework.permissions.AllowAny",
-        "rest_framework.permissions.IsAdminUser",
-        "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.IsAuthenticated", # 인증사 사용자
+        #"rest_framework.permissions.IsAdminUser", # 관리자
+
 
     ),
 
