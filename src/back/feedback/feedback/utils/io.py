c45de007a14a2baa8637b8470a32b8b1ec5c04af
Jaeo <ksw06137@gmail.com>
2022-05-13 11:46:30 +0900

    solve dependency

--- src/back/feedback/feedback/utils/io.py
+++ src/back/feedback/feedback/utils/io.py
@@ -1,14 +1,13 @@
-import boto3
-import librosa
-import soundfile as sf
-
-
-def read_video_from_s3(s3, bucket, object):
-    a = s3.generate_presigned_url(ClientMethod='get_object',
-                                  Params={'Bucket': bucket, 'Key': object})
+def get_url_from_s3(s3, bucket, key):
+    url = s3.generate_presigned_url(ClientMethod='get_object',
+                                  Params={'Bucket': bucket, 'Key': key})
+    return url
 
 
 def download_audio_from_s3(s3, bucket, object):
+    import librosa
+    import soundfile as sf
+
     s3.download_file(bucket, object, "/tmp/feedback.mp4")
     data, sr = librosa.load("/tmp/feedback.mp4")
     sf.write('/tmp/stt.wav', data, sr)
