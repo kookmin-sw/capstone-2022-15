c694c4547811ef40862ec73d825bd9075081356a
Jaeo <ksw06137@gmail.com>
2022-05-13 18:48:15 +0900

    upload without test main

--- src/back/feedback/main.py
+++ src/back/feedback/main.py
@@ -126,37 +126,3 @@ def lambda_handler(event, context):
     print("Upload result file")
 
 
-if __name__ == '__main__':
-    os.environ['FEEDBACK_TYPE'] = 'voice-analysis'
-    event = {
-  "Records": [
-    {
-      "EventSource": "aws:sns",
-      "EventVersion": "1.0",
-      "EventSubscriptionArn": "arn:aws:sns:us-east-1:{{{accountId}}}:ExampleTopic",
-      "Sns": {
-        "Type": "Notification",
-        "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
-        "TopicArn": "arn:aws:sns:us-east-1:123456789012:ExampleTopic",
-        "Subject": "example subject",
-        "Message": "interviewer-bucket\tuser_id_01/interview01/interviewer.mp4",
-        "Timestamp": "1970-01-01T00:00:00.000Z",
-        "SignatureVersion": "1",
-        "Signature": "EXAMPLE",
-        "SigningCertUrl": "EXAMPLE",
-        "UnsubscribeUrl": "EXAMPLE",
-        "MessageAttributes": {
-          "Test": {
-            "Type": "String",
-            "Value": "TestString"
-          },
-          "TestBinary": {
-            "Type": "Binary",
-            "Value": "TestBinary"
-          }
-        }
-      }
-    }
-  ]
-}
-    lambda_handler(event, "")
