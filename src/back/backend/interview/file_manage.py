a71fdfc19917e4fdee9886ae3df5df31525fb5cb
JiminK <jimin981005@gmail.com>
2022-05-13 20:35:09 +0900

    modify header token

--- src/back/backend/interview/file_manage.py
+++ src/back/backend/interview/file_manage.py
@@ -6,7 +6,7 @@ def select(field_id):
     random_n = random.randint(1, 5)
 
     if field_id == 0:
-        key = '{}/00{}.mp4'.format(field_id[0], field_list[random_n])
+        key = '{}/00{}.mp4'.format(field_list[field_id], field_list[random_n])
     else:
         key = 'job_interview/{}/00{}.mp4'.format(field_list[field_id], field_list[random_n])
 
