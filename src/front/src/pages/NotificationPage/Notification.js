8c9b5b7ccf10e0702102714e00814d223744e2f4
JiminK <jimin981005@gmail.com>
2022-05-16 13:55:04 +0900

    add page of notification for user

---
+++ src/front/src/pages/NotificationPage/Notification.js
@@ -0,0 +1,16 @@
+import './Notification.css';
+import Navbar from '../components/Navbar/Navbar';
+// import Footer from '../components/Footer';
+
+
+const PreInterview = () => {
+    return (
+        <div className="PreInterviewApp">
+            <Navbar/>
+            
+            {/* <Footer/> */}
+        </div>
+    );
+}
+
+export default PreInterview;
