c935a5bdf2d4618db0f16d2ac2f5182c5b95c09c
yoosun <39540525+usun813@users.noreply.github.com>
2022-05-16 23:02:04 +0900

    Update list.js

--- src/front/src/pages/MyPage/list.js
+++ src/front/src/pages/MyPage/list.js
@@ -5,64 +5,58 @@ import { Link } from 'react-router-dom';
 
 import axios from 'axios';
 
-const List = () => {
-    const [interview_id, getInterviewrID] = useState('');
-    const [field_id, getFieldID] = useState('')
-    const [interview_start, getInterviewStart] = useState('')
+class List extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data : [],
+    }
+  }
+
+  componentDidMount() {
+    this._getListData()
+  }
+
+  _getListData = async function() {
     const isTest = false;
     let getMypage = isTest
     ? `http://localhost:8000/accounts/mypage` // checkedId -> ques
     : `https://api.kmuin4u.com/accounts/mypage`; 
 
-    const data = {
-        interview_id:interview_id,
-        field_id:field_id,
-        interview_start:interview_start
+    const data_list = await axios(getMypage, {
+      method : 'GET',
+      headers:  {
+        'Authorization':'Token ' + window.localStorage.getItem('token')
     }
-    
-    axios({
-      url: getMypage, 
-      method: 'GET',
-      headers: {
-          'Authorization':'Token ' + window.localStorage.getItem('token')
-          
-      }
-    }).then(response => {
-      console.log("Mypage Get Success")
-        getInterviewrID(response.data.interview_id)
-        getFieldID(response.data.field_id)
-        getInterviewStart(response.data.interview_id)
-    })
-    .catch(error => {
-        console.log(error)
-        alert('error')
     })
 
+    this.setState({ data : data_list })
+  }
+
+  render() {
+    const list = this.state.data.data
 
     return (
       <div className='List'>
+
         <div className='list_grid list_tit'>
           <div className='acenter'> 제목 </div>
           <div className='acenter'> 날짜 </div>
         </div>
 
-        
-          {interview_id ? data.map( (el, key) => {
-            const GoFeedbackPage = '/accounts/feedback/' + interview_id;
-
+          {list ? list.map( (el, key) => {
+            const GoFeedbackPage = '/accounts/feedback/'+ el.interview_id;
             return(
               <div className='list_grid list_data' key={key}>
-                <div> <Link to={GoFeedbackPage}> {el.interview_id} </Link> </div>
+                <div> <Link to={GoFeedbackPage}> {el.field_id} </Link> </div>
                 <div> </div>
-                <div className='acenter'> {el.date.slice(0, 10)} </div>
+                <div className='acenter'> {el.interview_start.slice(0, 10)} </div>
               </div>
             )
           })
             : null }
-            <div> </div>
-          </div>
+      </div>
     );
+  }
 }
-
-
 export default List;
