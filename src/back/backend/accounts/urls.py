4f4cf205aff12e590803edf38bd62152187ab931
yeieun1213 <yeieun1213@kookmin.ac.kr>
2022-05-17 14:27:49 +0900

    accounts

--- src/back/backend/accounts/urls.py
+++ src/back/backend/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path("join", views.SignupView.as_view(), name="join"),
     path("login", views.LoginView.as_view(), name="login"),
+    path("mypage", views.MypageView.as_view(), name="mypage"),
+    path("feedback", views.FeedbackView.as_view(), name="feedback"),
 ]
