68e4c8e9472fe345f0bfccc060bdfd6789ec3081
yeieun1213 <yeieun1213@kookmin.ac.kr>
2022-05-19 18:10:02 +0900

    npz

--- src/back/backend/accounts/file_manage.py
+++ src/back/backend/accounts/file_manage.py
@@ -14,9 +14,9 @@ user-feedback-bucket -> bucket 이름
 def select(user_id, interview_id, question_n):
     user = f'user_id_{user_id}/interview_id_{interview_id}/result'
 
-    result_list = [f'{user}/face_movement_interview_{question_n}.npy',
-                   f'{user}/iris_movement_interview_{question_n}.npy',
-                   f'{user}/volume_interview_{question_n}.npy',
+    result_list = [f'{user}/face_movement_interview_{question_n}.npz',
+                   f'{user}/iris_movement_interview_{question_n}.npz',
+                   f'{user}/volume_interview_{question_n}.npz',
                    f'{user}/stt_interview_{question_n}.txt']
 
     return result_list
