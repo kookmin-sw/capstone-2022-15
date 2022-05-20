5cfacfcb0d305a5655f5360a403880b47cc81f5e
Jaeo <43405588+Grievle@users.noreply.github.com>
2022-05-20 17:09:29 +0900

    change to only data

--- src/back/feedback/feedback/video/face_movement.py
+++ src/back/feedback/feedback/video/face_movement.py
@@ -9,14 +9,11 @@ class FaceMovement:
         self.mp_drawing_styles = mp.solutions.drawing_styles
         self.mp_face_mesh = mp.solutions.face_mesh
         self.result_distance = []
-        self.duration = 0
 
     def eval(self, s3, bucket, key) -> np.array:
         path = download_video_from_s3(s3, bucket, key)
         cap = cv2.VideoCapture(path)
-        print('read url')
-        print(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        fps = cap.get(cv2.CAP_PROP_FPS)
+        print('read video')
         self.result_distance = []
         previous = np.array([0,0,0])
         with self.mp_face_mesh.FaceMesh(
@@ -32,21 +29,17 @@ class FaceMovement:
                     break
                 image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
                 results = face_mesh.process(image)
-                if int(cnt % (fps/2)) == 0:
-                    if results.multi_face_landmarks:
-                        for face_landmarks in results.multi_face_landmarks:
-                            landmark = face_landmarks.landmark[5]
-                            current = np.array([landmark.x, landmark.y, landmark.z])
-                            self.result_distance.append(np.sqrt(np.sum((current-previous)**2)))
-                            import copy
-                            previous = copy.deepcopy(current)
+                if results.multi_face_landmarks:
+                    for face_landmarks in results.multi_face_landmarks:
+                        landmark = face_landmarks.landmark[5]
+                        current = np.array([landmark.x, landmark.y, landmark.z])
+                        self.result_distance.append(np.sqrt(np.sum((current-previous)**2)))
+                        import copy
+                        previous = copy.deepcopy(current)
                 cnt += 1
-        self.duration = cnt / fps
 
         cap.release()
 
     def write(self, path='/tmp/face_movement.npz'):
-        time = np.array([i*0.5 for i in range(1, len(self.result_distance[1:])+1)])
-        print(time)
-        np.savez(path, time=time, data=self.result_distance[1:])
+        np.savez(path, data=self.result_distance[1:])
         return path
