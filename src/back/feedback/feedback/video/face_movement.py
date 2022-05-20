7cb873dd2c492dfff4d7137b543674a0ecef58f1
Jaeo <43405588+Grievle@users.noreply.github.com>
2022-05-20 15:05:25 +0900

    face movement with fps and cnt

--- src/back/feedback/feedback/video/face_movement.py
+++ src/back/feedback/feedback/video/face_movement.py
@@ -17,8 +17,6 @@ class FaceMovement:
         print('read url')
         print(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         fps = cap.get(cv2.CAP_PROP_FPS)
-        frame_num = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.duration = frame_num / fps
         self.result_distance = []
         previous = np.array([0,0,0])
         with self.mp_face_mesh.FaceMesh(
@@ -27,25 +25,28 @@ class FaceMovement:
                 min_detection_confidence=0.5,
                 min_tracking_confidence=0.5
             ) as face_mesh:
-            for cnt in range(frame_num):
+            cnt = 0
+            while cap.isOpened():
                 success, image = cap.read()
-
+                if not success:
+                    break
                 image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
                 results = face_mesh.process(image)
-                if int((cnt) % (fps/2)) == 0:
-                    print(cnt)
+                if int(cnt % (fps/2)) == 0:
                     if results.multi_face_landmarks:
                         for face_landmarks in results.multi_face_landmarks:
                             landmark = face_landmarks.landmark[5]
                             current = np.array([landmark.x, landmark.y, landmark.z])
-                            self.result_distance.append(np.sqrt(np.sum((current-previous)**2))*5)
+                            self.result_distance.append(np.sqrt(np.sum((current-previous)**2)))
                             import copy
                             previous = copy.deepcopy(current)
+                cnt += 1
+        self.duration = cnt / fps
 
         cap.release()
 
     def write(self, path='/tmp/face_movement.npz'):
-        time = np.linspace(0.5,int(self.duration), len(self.result_distance[1:]))
+        time = np.array([i*0.5 for i in range(1, len(self.result_distance[1:])+1)])
+        print(time)
         np.savez(path, time=time, data=self.result_distance[1:])
-        # print(time, self.result_distance[1:])
         return path
