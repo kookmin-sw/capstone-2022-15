19d64894cedeca22f8b29d19b8d83ec856bb1ec6
yoosun <39540525+usun813@users.noreply.github.com>
2022-05-20 21:28:35 +0900

    Update Feedback3.js

--- src/front/src/pages/MyPage/Feedback3.js
+++ src/front/src/pages/MyPage/Feedback3.js
@@ -1,148 +1,180 @@
 /* eslint-disable */
-import React, { useState, PureComponent, Component } from 'react';
+import React, { useState, PureComponent, Component, useEffect } from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer';
 import './Mypage.css';
 import { Link } from 'react-router-dom';
 import img_interviewer from '../images/img_interviewer.png';
-
+import SyncLoader from "react-spinners/SyncLoader";
 import axios from 'axios';
 export const Authentication = React.createContext(null);
-import {ScatterChart, Scatter, XAxis, YAxis, ZAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,} from 'recharts';
+import {LineChart, Line, ScatterChart, Scatter, XAxis, YAxis, ZAxis, 
+  CartesianGrid, Tooltip, Legend, ResponsiveContainer,} from 'recharts';
 
 
-const Feedback3 = () => {
-  const isTest = false;
-    let getFeedbackpage = isTest
-    ? `http://localhost:8000/accounts/feedback` // checkedId -> ques
-    : `https://api.kmuin4u.com/accounts/feedback`; 
-
-  axios({
-    url: getFeedbackpage, 
-    method: 'GET',
-    headers: {
-        'Authorization':'Token ' + window.localStorage.getItem('token')
-        
+class Feedback3 extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            data : [],
+            iris_movement : {},
+            face_movement : {},
+            volume_interview : {},
+            stt_interview : {},
+            videoUrl : '',
+            loading : true,
+            interview_id : 0
+        }
     }
-  }).then(response => {
-    console.log("Mypage Get Success")
-      setInterview_id(response.data.interview_id)
-      setQuestion_n(response.data.question_n)
-      setPreSignedUrl(response.data.interview_url);
-  })
-  .catch(error => {
-      console.log(error)
-      alert(' error')
-  })
 
+    componentDidMount(){
+        this._getListData()
+    }
 
-  return (
-    <div>
-        <Navbar/>
-        <Bar2/>
-        <MenuBox2/> 
-        <div className='mypage_footer_top2'>
-            <Footer/>
-        </div> 
-    </div>
-  );
-};
-
-
-  class MainFeedback extends Component{
-    render(){
-      const videoUrl = "https://www.youtube.com/embed/Y8JFxS1HlDo" 
-      //const videoUrl = user_id + interview_id + "/interview_video/interview" + interview_id+".mp4"
-      let chart_data1 = [
-        { x: 100, y: 200, z: 200 },
-        { x: 120, y: 100, z: 260 },
-        { x: 170, y: 300, z: 400 },
-        { x: 140, y: 250, z: 280 },
-        { x: 150, y: 400, z: 500 },
-        { x: 110, y: 280, z: 200 },
-      ];
-      const chart_data2 = [
-        { x: 200, y: 260, z: 240 },
-        { x: 240, y: 290, z: 220 },
-        { x: 190, y: 290, z: 250 },
-        { x: 198, y: 250, z: 210 },
-        { x: 180, y: 280, z: 260 },
-        { x: 210, y: 220, z: 230 },
-      ];
+    _getListData = async function(){
+      const isTest = false;
+      const interview_id = 1; // --------------- 변경
+      const question_n = 0;
+        let getFeedbackpage = isTest
+        ? `http://localhost:8000/accounts/feedback/${interview_id}/${question_n}` // checkedId -> ques
+        : `https://api.kmuin4u.com/accounts/feedback/${interview_id}/${question_n}`;
       
+      const data_list = await axios(getFeedbackpage,{
+        method : 'GET',
+        headers : {
+            'Authorization':'Token ' + window.localStorage.getItem('token')
+        }
+      })
+      this.setState({data: data_list})
+    }
 
-      return(
-        <div>
+    render(){
+        const list = this.state.data.data
+        console.log("Mypage Get Success")
+        console.log(list)
+
+    if (!list) return (
+        <div></div>
+    )
+    if (list){
+    console.log("video: ", list.interviewee_url)
+    console.log(list.interviewee_url.interviewee_url)
+        return (
+            <div>
+                <Navbar/>
+                <Bar2/>
+
+      <div className='Menu-box' style={{height: '2300px'}}>
+          <div className='mypage_footer_top2'>
+            <Footer/>
+          </div> 
+          <div onClick={()=>console.log("마이 페이지(연습목록)로 페이지 변경")}>
+              <Link to="/mypage" className='Menu-txt22'>
+              연습목록
+              </Link>
+          </div>
+
+          <div onClick={()=>console.log("질문 1 Feedback")}>
+              <Link to="/feedback1/" className='Menu-txt3' style={{top:'14vh'}}>
+              &nbsp;&nbsp;질문 1
+              </Link>
+          </div>
+
+          <div onClick={()=>console.log("질문 2 Feedback")}>
+              <Link to="/feedback2/" className='Menu-txt3' style={{top:'20vh'}}>
+              &nbsp;&nbsp;질문 2
+              </Link>
+          </div>
+
+          <div onClick={()=>console.log("질문 3 Feedback") }>
+              <Link to="/feedback3/" className='Menu-txt3' style={{top:'26vh'}}>
+              &nbsp;&nbsp;질문 3
+              </Link>
+          </div>
+
+
+          <div className='Main-box'>
+            <div>
           <div className='Feedback-Q'> Q3 </div>
-          <div className='Feedback-txt'style={{top:'5.2vh'}}>
+          <div className='Feedback-txt'style={{top:'50px'}}>
                 🔹 Video Check
           </div>
           <div className="Interviewer-section">
-                <iframe width="700vw" height="394vh" src={videoUrl} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>              
+                {/*<iframe width="700vw" height="394vh" src={list.interviewee_url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>*/}
+              {list.interviewee_url!=='' && <video width="80%" height="80%" autoPlay={true} controls>
+                        <source src={list.interviewee_url}/>
+                  </video>}
+                    {list.interviewee_url==='' && <SyncLoader color={'blue'} loading={true} css={override} size={30} />}
           </div>
-  
-  
+
+
   {/*나의 답변*/}
-          <div className='Feedback-txt' style={{top:'17vh'}}>
+          <div className='Feedback-txt' style={{top:'117px'}}>
                 🔹 나의 답변
             <div className="Stt">
-              여기에 STT 내용 <br/>
-              🐟🐠🐡🦐🦑🐙🦞🐬🐳🐋🦀🐧🐚<br/>
-              인퓨는 가상 생성된 모습의 면접관이 입모양을 움직이며 음성으로 질문을 전달할 수 있어, 면접관이 존재하지 않고 텍스트와 음성만으로 질문을 확인하여 연습하는 기존 면접 연습 서비스와 다르게 더욱 현장감있는 면접 연습을 제공한다. 인퓨는 가상 생성된 모습의 면접관이 입모양을 움직이며 음성으로 질문을 전달할 수 있어, 면접관이 존재하지 않고 텍스트와 음성만으로 질문을 확인하여 연습하는 기존 면접 연습 서비스와 다르게 더욱 현장감있는 면접 연습을 제공한다. 인퓨는 가상 생성된 모습의 면접관이 입모양을 움직이며 음성으로 질문을 전달할 수 있어, 면접관이 존재하지 않고 텍스트와 음성만으로 질문을 확인하여 연습하는 기존 면접 연습 서비스와 다르게 더욱 현장감있는 면접 연습을 제공한다.  
-  
+              {list.stt_interview.slice(9, -2)}<br/>
             </div>
           </div>
-          
-  
+
+
   {/*시선 처리 차트*/}
-          <div className='Feedback-txt'style={{top:'32vh'}}>
+          <div className='Feedback-txt'style={{top:'194px'}}>
                 🔹 시선 처리
               <div className='ChartBackground'>
                 <img src={img_interviewer}/>
               </div>
-              
-              <div style={{ width: '46.5vw', height: '51.3vh',  left:'14vw',position:'absolute'}}>
-                <Rechart chart_data= {chart_data1}  />
+
+              <div style={{ width: '594px', height: '313px',  left:'150px',position:'absolute'}}>
+                <Scatter_chart_iris scatter_data= {list.volume_interview}  />
               </div>
-              
+
           </div>
-          
-  
-  
+
+
+
   {/*머리 움직임 차트*/}
-          <div className='Feedback-txt' style={{top:'100vh'}}>
+          <div className='Feedback-txt' style={{top:'605px'}}>
                 🔹 머리 움직임
-              <div style={{ width: '46.5vw', height: '51.3vh',  left:'14vw',position:'absolute'}}>
-                <Rechart chart_data= {chart_data2}  />
-                {/*<Rechart chart_data= {response.data.face_movement}  /> */}
+              <div style={{ overflowX:'scroll',width: '594px', height: '330px',  left:'150px',position:'absolute'}}>  
+              <div style={{ width: '800px', height: '300px'}}>  
+                <Line_chart_face line_data= {list.volume_interview}  />
+              </div>
               </div>
           </div>
-  
-  
-  {/*목소리 크기 차트*/}
-          <div className='Feedback-txt' style={{top:'168vh'}}>
+
+
+  {/*목소리 크기 차트 react horizontal scrolling?*/}
+          <div className='Feedback-txt' style={{top:'968px'}}>
                 🔹 목소리 크기
-              <div style={{ width: '46.5vw', height: '51.3vh',  left:'14vw',position:'absolute'}}>
-                <Rechart chart_data= {chart_data1}/>
-                {/*<Rechart chart_data= {response.data.volumn_interview}  /> */}
+              <div style={{ overflowX:'scroll',width: '594px', height: '330px',  left:'150px',position:'absolute'}}>  
+              <div style={{ width: '800px', height: '300px'}}>  
+              {/*<div style={{ width: '594px', height: '300px',  left:'150px',position:'absolute'}}>*/}
+                <Line_chart_volume line_data= {list.volume_interview}  />
               </div>
-          </div>  
-        </div>  
-      );
-    }
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    );
   }
+ }
+}
 
 
-  
 
-const Rechart = ({
-  chart_data
+
+
+{/*********************  Scatter Chart - 시선처리 차트 ********************/}
+const Scatter_chart_iris = ({
+  scatter_data
   }) => {
   return (
     <ResponsiveContainer width="100%" height="100%">
     <ScatterChart
-      width={500}
-      height={300}
+      width={'500px'}
+      height={'300px'}
       margin={{
           top: 5,
           right: 30,
@@ -151,18 +183,90 @@ const Rechart = ({
       }}
     >
       <CartesianGrid />
-      <XAxis type="number" dataKey="x" name="stature" unit="cm" />
-      <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+      <XAxis type="number" dataKey="x" name="x" unit="cm" />
+      <YAxis type="number" dataKey="y" name="y" unit="kg" />
       <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-      <Scatter name="A school" data={chart_data} fill="#5B7EFB" />
+      <Scatter name="A school" data={scatter_data} fill="#5B7EFB" />
     </ScatterChart>
   </ResponsiveContainer>
   );
 }
 
+
+{/*********************  Line Chart - 머리 움직임 차트 ********************/}
+const Line_chart_face = ({
+  line_data
+  }) => {console.log("line", line_data)
+  return (
+      <ResponsiveContainer width="100%" height="100%">
+         <LineChart
+            width={'500px'}
+            height={'300px'}
+            data={line_data}
+            margin={{
+                      top: 5,
+                      right: 30,
+                      left: 20,
+                      bottom: 5,
+                    }}
+            >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" /><YAxis /> <Tooltip /> <Legend />
+            <Line
+              type="monotone"
+              dataKey="y"
+              name="머리움직임"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+            {/*<Line type="monotone" dataKey="y" stroke="#82ca9d"/>*/}
+          </LineChart>
+        </ResponsiveContainer>
+
+  );
+}
+
+
+{/*********************  Line Chart - 목소리 크기 차트 ********************/}
+const Line_chart_volume = ({
+  line_data
+  }) => {console.log("line", line_data)
+  return (
+      <ResponsiveContainer width="100%" height="100%">
+         <LineChart
+            width={'500px'}
+            height={'300px'}
+            data={line_data}
+            margin={{
+                      top: 5,
+                      right: 30,
+                      left: 20,
+                      bottom: 5,
+                    }}
+            >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name"/><YAxis /> <Tooltip /> <Legend />
+            <Line
+              type="monotone"
+              dataKey="y"
+              name="크기"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+            {/*<Line type="monotone" dataKey="y" stroke="#82ca9d"/>*/}
+          </LineChart>
+        </ResponsiveContainer>
+
+  );
+}
+
+
+
+
 class Bar2 extends Component{
   render(){
-    
+//function Bar2(props){
+
     return(
       <div className='Bar'>
           My Page - 피드백
@@ -171,40 +275,15 @@ class Bar2 extends Component{
   }
 }
 
-class MenuBox2 extends Component{
-  render(){
-    return(
-      <div className='Menu-box' style={{height: '377vh'}}>
-          <div onClick={()=>console.log("마이 페이지(연습목록)로 페이지 변경")}>
-              <Link to="/mypage" className='Menu-txt22'>
-              연습목록
-              </Link>
-          </div>
 
-          <div onClick={()=>console.log("질문 1 Feedback")}>
-              <Link to="/feedback1/*" className='Menu-txt3' style={{top:'14vh'}}>
-              &nbsp;&nbsp;질문 1
-              </Link>
-          </div>
-
-          <div onClick={()=>console.log("질문 2 Feedback")}>
-              <Link to="/feedback2/*" className='Menu-txt3' style={{top:'20vh'}}>
-              &nbsp;&nbsp;질문 2
-              </Link>
-          </div>
-
-          <div onClick={()=>console.log("질문 3 Feedback") }>
-              <Link to="/feedback3/*" className='Menu-txt3' style={{top:'26vh'}}>
-              &nbsp;&nbsp;질문 3
-              </Link>
-          </div>
-
-      
-          <div className='Main-box'>
-            <MainFeedback/>
-        </div>
-      </div>
-    );
-  }
+const override = {
+  margin: 'auto',
+  borderColor: 'red',
+  justifyContent: 'center',
+  display: 'flex',
+  alignItems: 'center',
+  height: '98vh',
+  width: '100%'
 }
+
 export default Feedback3;
