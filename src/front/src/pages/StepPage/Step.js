2f682bc5b523a3c315e71317e1c1a2df9bbc0f33
JiminK <jimin981005@gmail.com>
2022-05-23 01:50:19 +0900

    step1 and step2

--- src/front/src/pages/StepPage/Step.js
+++ src/front/src/pages/StepPage/Step.js
@@ -1,5 +1,8 @@
 import './Step.css';
 import Navbar from '../components/Navbar/Navbar';
+import step1 from '../images/step1.png'
+import step2 from '../images/step2.jpg'
+
 
 function clickHandler(e) {
     window.location.href = "/preinterview"
@@ -7,7 +10,7 @@ function clickHandler(e) {
 
 const Step = () => {
     return (
-        <div className="NotificationApp">
+        <div className="StepApp">
             <Navbar/>
             <div className="description-layout">
                 <span className="description-title">
@@ -27,30 +30,44 @@ const Step = () => {
                 </span>
             </div>
             <div className="notification-layout1">
-                <div className="notification-title">
-                    Step 1 <br></br>
-                    왼쪽 상단의 카메라, 마이크 접근 허용을 눌러 화상 기기를 연결한다.<br></br>
+                <div className="step-title">
+                    STEP 1
                 </div>
-                <div className="notification-title">
-                    Step 2 <br></br>
-                    연습하고 싶은 면접의 종류를 선택한다.<br></br>
+                <div className="step-content">
+                    왼쪽 상단의 카메라, 마이크 접근 허용을 눌러 화상 기기를 연결한다.
+                    <img src={step1} className="step1"></img>
                 </div>
-                <div className="notification-title">
-                    Step 3 <br></br>
-                    Start 버튼을 누르고 면접 연습 창이 나오면 면접 시작 버튼을 눌러 연습을 시작한다.<br></br>
+                <div className="step-title">
+                    STEP 2 
                 </div>
-                <div className="notification-title">
-                    Step 4 <br></br>
-                    면접관의 질문이 끝나면 대답을 시작한다. 대답이 완료되면 대답 완료 버튼을 누른다.<br></br>
+                <div className="step-content">
+                    연습하고 싶은 면접의 종류를 선택한다.
+                    <img src={step2} className="step2"></img>
                 </div>
-                <div className="notification-title">
-                    Step 5 <br></br>
-                    다음 질문 버튼을 눌러 면접을 진행한다.<br></br>
+                <div className="step-title">
+                    STEP 3 
                 </div>
-                <div className="notification-title">
-                    Step 6 <br></br>
-                    한 면접당 3개의 질문이 주어지고, 질문이 모두 끝나면 면접 종료 버튼을 눌러 면접을 종료한다.<br></br>
-                    면접 종료 버튼을 누르면 면접이 종료되고 마이페이지로 이동한다.<br></br>
+                <div className="step-content">
+                    Start 버튼을 누르고 면접 연습 창이 나오면 면접 시작 버튼을 눌러 연습을 시작한다.
+                </div>
+                <div className="step-title">
+                    STEP 4 
+                </div>
+                <div className="step-content">
+                    면접관의 질문이 끝나면 대답을 시작한다. 대답이 완료되면 대답 완료 버튼을 누른다.
+                </div>
+                <div className="step-title">
+                    STEP 5 
+                </div>
+                <div className="step-content">
+                    다음 질문 버튼을 눌러 면접을 진행한다.
+                </div>
+                <div className="step-title">
+                    STEP 6 
+                </div>
+                <div className="step-content">
+                    한 면접당 3개의 질문이 주어지고, 질문이 모두 끝나면 면접 종료 버튼을 눌러 면접을 종료한다.
+                    면접 종료 버튼을 누르면 면접이 종료되고 마이페이지로 이동한다.
                 </div>
                 <div className="next-button-layout">
                     {/* <Link to="/preinterview" > */}
