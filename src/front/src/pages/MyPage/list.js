b9ba0d78edea058fb9ec91e92c807131676791ed
yeieun1213 <yeieun1213@kookmin.ac.kr>
2022-05-24 14:13:13 +0900

    front

--- src/front/src/pages/MyPage/list.js
+++ src/front/src/pages/MyPage/list.js
@@ -60,7 +60,7 @@ class List extends Component {
             const GoFeedbackPage = '/feedback1/'
             return(
               <div className='list_grid list_data' key={key}>
-                <div className='acenter list_data'> {el+1}</div>
+                <div className='acenter list_data'> {key+1}</div>
                 <div className='acenter list_data'> <Link to={GoFeedbackPage}> {interviewTypename[el.fields.field_id]} </Link> </div>
                 <div className='acenter list_data'> {el.fields.interview_date.slice(0,10)} </div>
               </div>
