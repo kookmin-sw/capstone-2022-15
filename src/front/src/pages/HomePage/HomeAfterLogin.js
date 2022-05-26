0c08687d14ac90727758ef7b2db10b12c874f3b7
JiminK <jimin981005@gmail.com>
2022-05-26 17:04:59 +0900

    modify css for home page

--- src/front/src/pages/HomePage/HomeAfterLogin.js
+++ src/front/src/pages/HomePage/HomeAfterLogin.js
@@ -82,7 +82,7 @@ class Middle extends Component{
             실제 면접과 유사한 방식으로 연습할 수 있습니다.
           </span>
           <div onClick={()=>console.log("면접연습 페이지로 페이지 변경")}>
-                <Link to ="/preinterview" className="BT-join" style={{left:'68.2vw', top:'52vh'}}>
+                <Link to ="/preinterview" className="BT-join" style={{left:'60.5vw', top:'50vh'}}>
                   연습시작
                 </Link>
           </div>
@@ -145,7 +145,7 @@ class Footer_pink extends Component{
             </span>
           </div>
           <div onClick={()=>console.log("로그인(회원가입) 페이지로 페이지 변경 이벤트 추가")}>
-            <Link to ="/preinterview" className="BT-join" style={{left:'74vw', top:'33vh'}}>
+            <Link to ="/preinterview" className="BT-join" style={{left:'74vw', top:'26vh'}}>
                  연습시작
             </Link>
           </div>
