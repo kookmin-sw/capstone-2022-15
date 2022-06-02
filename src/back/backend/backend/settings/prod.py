b1593c312a1cdbbd3cff8d024c55f541f81c3f48
yeieun1213 <yeieun1213@kookmin.ac.kr>
2022-06-02 17:17:57 +0900

    prod

--- src/back/backend/backend/settings/prod.py
+++ src/back/backend/backend/settings/prod.py
@@ -3,7 +3,7 @@ from .common import *
 
 INTERNAL_IPS = ["127.0.0.1"]
 
-CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
+CORS_ORIGIN_WHITELIST = ["http://api.kmuin4u.com"]
 
 # Database
 DATABASES = {
